41fbc0772e4409ac50616f749917b53a79514c78
Tychiq <tychique.allagbe77@gmail.com>
2025-03-04 16:36:12 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  vite : {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   plugins: [react(), tailwindcss(),],
 })
