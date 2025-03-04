1492692b0e48e87c35be311d7561d1f88c5acf77
Tychiq <tychique.allagbe77@gmail.com>
2025-03-04 21:13:20 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+
 })
+export default {
+    build: {
+        rollupOptions: {
+            external: ['node_modules']
+        }
+    }
+};
+
